9fa1dc007abbea0f46900f459e1eb744f3cda78c
day-5 <day-5@LAPTOP-JHTEA5PL>
2022-12-21 14:53:17 +0500

    Добавьте файлы проекта.

---
+++ Paleondrom/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Paleondrom
+{
+    class Program
+    {
+
+        internal bool CheckPaleondrom() { 
+            bool result = false;
+            Console.WriteLine("Введите слово/число");
+            string InputText = Console.ReadLine();
+            string ReverseText = new string(InputText.Reverse().ToArray());
+            if (InputText == ReverseText)
+            {
+                Console.WriteLine("Это Палиндром");
+                result = true; 
+            }
+            else
+            {
+                Console.WriteLine("Это не Палиндром");
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            // На вход получаем слово, число 
+            // Переворачиваем и сравниваем
+            Program Test = new Program();
+            Test.CheckPaleondrom();
+
+
+
+
+
+        }
+    }
+}
